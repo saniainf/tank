0d8d817d5e8099e7126b3ba9d46f024d6a87b670
Inf <saniainf@gmail.com>
2014-03-04 13:22:35 +0700

    first commit

---
+++ Assets/Scripts/camera2D.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera2D : MonoBehaviour
+{
+
+    public float pixels2units = 100.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera.orthographicSize = (Screen.height / pixels2units / 2.0f);
+    }
+}
---
+++ Assets/Scripts/tstScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tstScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var value = Input.GetAxis("Horizontal");
+        Debug.Log(value);
+    }
+}
